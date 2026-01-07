8d695a9dea015c16991571b606349fc2cbdd56d2
mina <minamalak761@gmail.com>
2026-01-07 17:21:06 +0200

    JWT , RefreshToken , Logout

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const SALT_ROUNDS = 10;
 
 const userSchema = new mongoose.Schema(
   {
@@ -36,5 +39,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
